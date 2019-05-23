e66fb7772ff30ef7e124215814766a1dd709bb05
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 16:51:30 +0530

    Add files via upload

---
+++ 17.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+int main()
+{
+    int n,m,r, flag= 0;
+    scanf("%d", &n);
+    m= n;
+
+    while (m!= 0)
+    {
+        r= m%10;
+        flag += r*r*r;
+        m /= 10;
+    }
+    if(flag == n)
+        printf("yes ");
+    else
+        printf("no ");
+
+    return 0;
+}
